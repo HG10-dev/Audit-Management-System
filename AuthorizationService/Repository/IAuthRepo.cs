eadeaf21eced793ceb15910de690237bb74d1b65
HG10-dev <62341829+HG10-dev@users.noreply.github.com>
2022-07-06 17:33:11 +0530

    Update IAuthRepo.cs

--- AuthorizationService/Repository/IAuthRepo.cs
+++ AuthorizationService/Repository/IAuthRepo.cs
@@ -3,8 +3,7 @@
 namespace AuthorizationService.Repository
 {
     public abstract class IAuthRepo
-    {
-        public abstract AuthCredentials Authenticate(AuthCredentials cred);
+    {        
         public abstract string GenerateJSONWebToken(AuthCredentials cred);
     }
 }
